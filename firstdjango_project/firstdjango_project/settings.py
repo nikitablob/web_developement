749b2a3f7bd64f4b3f75cfd942153556cd1eac5f
Nikita Yadav <nikita.yadav@candelatech.com>
2022-01-15 23:23:13 +0530

    templates

--- firstdjango_project/firstdjango_project/settings.py
+++ firstdjango_project/firstdjango_project/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# print((os.path.abspath(__file__)))
+# print(Path(__file__).resolve().parent.parent)
+# print(os.path.join(BASE_DIR, "templates"))
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'firstdjango_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
